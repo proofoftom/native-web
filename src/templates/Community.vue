b6b4737bcde910d155fec1a4d11326c87c59e29f
Tommy Cox <tommy@nativeproject.one>
2019-04-16 03:01:02 -0600

    Add MVP Community Template
    WIP for #5

---
+++ src/templates/Community.vue
@@ -0,0 +1,53 @@
+<template>
+  <Layout :title="$page.community.title">
+    <div class="content">
+      <img class="avatar" :src="$page.community.icon"/>
+      <h2>{{ $page.community.name }}</h2>
+      <div class="location" v-html="$page.community.location" /> |
+      <div class="memberCount">{{ $page.community.memberCount }} Members</div>
+      <div class="communityPurpose" v-html="$page.community.communityPurpose" />
+      <div class="subtitle" v-html="$page.community.subtitle" />
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query community ($path: String!) {
+  community: community (path: $path) {
+    title
+    icon
+    name
+    location
+    memberCount
+    communityPurpose
+    subtitle
+  }
+}
+</page-query>
+
+<script>
+import Layout from '~/layouts/Default.vue'
+export default {
+  components: {
+    Layout
+  },
+  metaInfo () {
+    return {
+      title: this.$page.community.name
+    }
+  }
+}
+</script>
+
+<style scoped>
+.location, .memberCount {
+  display: inline;
+}
+
+.subtitle {
+  padding: 5px;
+  border: 1px solid darkslategray;
+  border-radius: 5px;
+  display: inline;
+}
+</style>
