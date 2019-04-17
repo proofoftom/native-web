4bf8c06b9dbbb06cb3b4ca57e292ca27c6bd4fa6
Tommy Cox <tommy@nativeproject.one>
2019-04-17 17:54:53 -0600

    Move CommunityCard to its own component

---
+++ src/components/CommunityCard.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="communityCard">
+    <img class="hero" :src="community.image" width="100%" />
+    <h3>{{ community.title }}</h3>
+    <div class="communityInfo">
+      <div class="location" v-html="community.location" /> |
+      <div class="memberCount">{{ community.memberCount }} Members</div>
+    </div>
+    <div class="communityPurpose" v-html="community.communityPurpose" />
+    <div class="subtitles">
+      <div class="subtitle" v-html="community.subtitle" />
+    </div>
+    <div class="actions">
+      <!-- Todo: Check for auth and swap out link for Join Community link -->
+      <button @click="routerPush('login')">Join | $10.00</button>
+      <button @click="routerPush(community.path)">Go</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Community',
+  props: {
+    community: {}
+  },
+  methods: {
+    routerPush(path) {
+      this.$router.push(path)
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+.communityCard {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  min-width: 295px;
+  padding: 10px;
+  margin: 10px;
+  border: 1px solid darkslategray;
+  border-radius: 5px;
+  box-sizing: border-box;
+}
+
+div {
+  margin: 5px;
+}
+
+.location,
+.memberCount {
+  display: inline;
+}
+
+.subtitle {
+  padding: 5px;
+  border: 1px solid darkslategray;
+  border-radius: 5px;
+  display: inline;
+}
+
+.actions {
+  display: flex;
+  justify-content: space-between;
+  align-content: stretch;
+  align-items: flex-end;
+  flex: 2;
+}
+
+button {
+  margin-top: 5px;
+  padding: 5px;
+}
+
+@media (max-width: 760px) {
+  .communityCard {
+    margin: 0 0 1rem 0;
+  }
+}
+</style>
--- src/pages/Communities.vue
+++ src/pages/Communities.vue
@@ -2,19 +2,11 @@
   <Layout>
     <h1>Explore Communities</h1>
     <div class="communities">
-      <div v-for="community in $page.communities.edges" :key="community.node.id" class="community">
-        <img class="hero" :src="community.node.image" width="100%" />
-        <h3>{{ community.node.name }}</h3>
-        <div class="location" v-html="community.node.location" /> |
-        <div class="memberCount">{{ community.node.memberCount }} Members</div>
-        <div class="communityPurpose" v-html="community.node.communityPurpose" />
-        <div class="subtitle" v-html="community.node.subtitle" />
-        <div class="actions">
-          <!-- Todo: Check for auth and swap out link for Join Community link -->
-          <button @click="routerPush('login')">Join | $10.00</button>
-          <button @click="routerPush(community.node.path)">Go</button>
-        </div>
-      </div>
+      <CommunityCard
+        v-for="community in $page.communities.edges"
+        :key="community.node.id"
+        :community="community.node"
+      />
     </div>
   </Layout>
 </template>
@@ -24,7 +16,7 @@ query Communities {
   communities: allCommunity {
     edges {
       node {
-        name
+        title
         image
         location
         memberCount
@@ -38,68 +30,30 @@ query Communities {
 </page-query>
 
 <script>
+import CommunityCard from "~/components/CommunityCard";
+
 export default {
   metaInfo: {
     title: 'Communities'
   },
-  methods: {
-    routerPush(path) {
-      this.$router.push(path)
-    },
-  }
+  components: {
+    CommunityCard,
+  },
 }
 </script>
 
 <style scoped>
 .communities {
   display: flex;
-  align-items: flex-start;
+  align-items: stretch;
   flex-direction: row;
   justify-content: space-between;
-  max-width: 760px;
-}
-
-.community {
-  width: 30%;
-  padding: 10px;
-  border: 1px solid darkslategray;
-  border-radius: 5px;
-  box-sizing: border-box;
-}
-
-.community div {
-  margin: 5px;
-}
-
-.location, .memberCount {
-  display: inline;
-}
-
-.subtitle {
-  padding: 5px;
-  border: 1px solid darkslategray;
-  border-radius: 5px;
-  display: inline;
-}
-
-.community .actions {
-  display: flex;
-  justify-content: space-between;
-}
-
-.community button {
-  margin-top: 5px;
-  padding: 5px;
+  flex-wrap: wrap;
 }
 
 @media (max-width: 760px) {
   .communities {
     flex-direction: column;
-  }
-
-  .community {
-    width: 100%;
-    margin-bottom: 1rem;
   }
 }
 </style>
