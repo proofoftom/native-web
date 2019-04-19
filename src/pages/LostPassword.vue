8c7118a660b26b966dfdbbeab265c858977d4564
Tommy Cox <tommy@nativeproject.one>
2019-04-19 04:41:59 -0600

    Added Lost Password page/form

---
+++ src/pages/LostPassword.vue
@@ -0,0 +1,19 @@
+<template>
+  <Layout>
+    <h1>Lost Password</h1>
+    <LostPasswordForm />
+  </Layout>
+</template>
+
+<script>
+import LostPasswordForm from "~/components/forms/LostPasswordForm";
+
+export default {
+  metaInfo: {
+    title: "Lost Password"
+  },
+  components: {
+    LostPasswordForm
+  }
+};
+</script>
