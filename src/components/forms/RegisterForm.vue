080fd8e4e46cbc6b7c8bbbb3e0733b7f1e48e6ce
Tommy Cox <tommy@nativeproject.one>
2019-04-19 00:32:27 -0600

    Register form

---
+++ src/components/forms/RegisterForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-layout>
+    <v-flex xs12 sm6>
+      <v-form >
+        <g-link to="/login" class="subheading">Sign in with another account</g-link>
+        <v-text-field label="Username" v-model="username"></v-text-field>
+        <v-text-field label="Email" v-model="email"></v-text-field>
+        <v-text-field label="Password" v-model="password"></v-text-field>
+        <v-text-field label="Confirm Password" v-model="passwordConfirm"></v-text-field>
+        <v-spacer></v-spacer>
+        <v-btn class="mt-4" @click="createUser()">Submit</v-btn>
+      </v-form>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        username: '',
+        email: '',
+        password: '',
+        passwordConfirm: ''
+      }
+    },
+    methods: {
+      createUser() {
+        // Do some user auth stuff here...
+      }
+    },
+  }
+</script>
---
+++ src/pages/Register.vue
@@ -0,0 +1,19 @@
+<template>
+  <Layout>
+    <h1>Sign Up</h1>
+    <RegisterForm />
+  </Layout>
+</template>
+
+<script>
+import RegisterForm from '~/components/forms/RegisterForm'
+
+export default {
+  metaInfo: {
+    title: 'Sign Up'
+  },
+  components: {
+    RegisterForm
+  }
+}
+</script>
