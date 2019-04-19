56449e5519fda1ed409ccd1638e73491a5fb07f0
Tommy Cox <tommy@nativeproject.one>
2019-04-18 20:43:14 -0600

    Wrap card info in container

--- src/components/CommunityCard.vue
+++ src/components/CommunityCard.vue
@@ -2,20 +2,22 @@
   <v-flex xs12 sm6 md4>
     <v-card class="community-card">
       <img :src="community.image" width="100%" />
-      <h3>{{ community.title }}</h3>
-      <div class="community-info">
-        <div class="location" v-html="community.location" /> |
-        <div class="member-count">{{ community.memberCount }} Members</div>
-      </div>
-      <div class="community-purpose" v-html="community.communityPurpose" />
-      <div class="subtitles">
-        <div class="subtitle" v-html="community.subtitle" />
-      </div>
-      <div class="actions">
-        <!-- Todo: Check for auth and swap out link for Join Community link -->
-        <v-btn @click="routerPush('login')">Join | $10.00</v-btn>
-        <v-btn @click="routerPush(community.path)">Go</v-btn>
-      </div>
+      <v-container>
+        <h3>{{ community.title }}</h3>
+        <div class="community-info">
+          <div class="location" v-html="community.location" /> |
+          <div class="member-count">{{ community.memberCount }} Members</div>
+        </div>
+        <div class="community-purpose" v-html="community.communityPurpose" />
+        <div class="subtitles">
+          <div class="subtitle" v-html="community.subtitle" />
+        </div>
+        <div class="actions">
+          <!-- Todo: Check for auth and swap out link for Join Community link -->
+          <v-btn @click="routerPush('login')">Join | $10.00</v-btn>
+          <v-btn @click="routerPush(community.path)">Go</v-btn>
+        </div>
+      </v-container>
     </v-card>
   </v-flex>
 </template>
