31437dd6b8d5da793015705b655e629f45764f1d
Tommy Cox <tommy@nativeproject.one>
2019-06-22 13:05:19 -0600

    Let's just use divs

--- src/components/roadmap/StatusGroup.vue
+++ src/components/roadmap/StatusGroup.vue
@@ -1,8 +1,8 @@
 <template>
   <v-timeline-item small color="#549387">
-    <span class="subheading grey--text text--darken-2">{{ this.$props.title }}</span>
-    <div class="mt-1 grey--text">
-      <span class="body-2">{{ this.$props.description }}</span>
+    <div class="subheading grey--text text--darken-2">{{ this.$props.title }}</div>
+    <div class="mt-1 grey--text body-2">
+      {{ this.$props.description }}
     </div>
     <roadmap-card
       v-for="(feature, index) in this.$props.features"
