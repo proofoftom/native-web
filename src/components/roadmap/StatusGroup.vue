a44513dbc24652d260c6affd7c2adfd4c5857394
Tommy Cox <tommy@nativeproject.one>
2019-06-21 23:27:50 -0600

    Add StatusGroup component containing RoadmapCards

---
+++ src/components/roadmap/StatusGroup.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-timeline-item small color="#549387">
+    <div class="mt-3">
+      <span class="subheading grey--text text--darken-2">{{ this.$props.title }}</span>
+    </div>
+    <roadmap-card
+      v-for="(feature, index) in this.$props.features"
+      :key="index"
+      :feature="feature"
+    />
+  </v-timeline-item>
+</template>
+
+<script>
+import RoadmapCard from "~/components/cards/RoadmapCard";
+  export default {
+    props: {
+      title: {
+        type: String
+      },
+      features: {
+        type: Array
+      }
+    },
+    components: {
+      RoadmapCard,
+    },
+  }
+</script>
